0605eb132a5baeea08279dfe3515bcfe9cb8e3bb
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-05-24 16:33:52 +0300

    add: routes

--- apps/nn/yolov8/fastapi/routes.py
+++ apps/nn/yolov8/fastapi/routes.py
@@ -7,20 +7,27 @@ from ..model import YOLOv8Wrapper
 from ...serve import ServedModel
 from .models import Prediction, BBox
 from ....utility.split_video import split_video_by_frames
+
+
 router = APIRouter(
     prefix="/yolo",
     tags=["yolo"],
 )
 
-_ServedYOLO = ServedModel(
-    YOLOv8Wrapper,
-    pt_path="./models/1200.pt",
-    gpu=1,
-    enable_printing=True
-)
+
+def serve_yolo(cfg):
+    global _ServedYOLO
+    
+    _ServedYOLO = ServedModel(
+        YOLOv8Wrapper,
+        **cfg,
+        enable_printing=True
+    )
 
 @router.post("/video")
 async def detect_video(video: UploadFile = File(...)) -> list[Prediction]:
+    global _ServedYOLO
+
     with tempfile.TemporaryDirectory() as tempdir:
         shutil.copyfileobj(video.file, open(f"{tempdir}/video.mp4", "wb"))
         split_video_by_frames(f"{tempdir}/video.mp4", take_each_n=1, output_collection=f"{tempdir}/frames", verbose=False)
