2bfbd7e6eac14d8b1c7c26ac43ac88001e57a272
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-05-28 17:44:44 +0300

    more logs

--- apps/fastapi/routes/score_card.py
+++ apps/fastapi/routes/score_card.py
@@ -165,8 +165,9 @@ def _process_video_file_for_score_card(video_path: str) -> dict:
     inputs = torch.Tensor(
         np.hstack((embeddings, np.array(vectors),))
     )
-    
+    logger.info(f"{video_path}, yolo data prepared")
     logits = classifier(inputs)
+    logger.info(f"{video_path}, logits: {logits.shape}")
     classification = predict(embeddings, logits.detach().cpu(), logits=True)
     logger.info(f"{video_path}, classfied")
     yolo_output = assign_to_rooms(classification, yolo_output)
